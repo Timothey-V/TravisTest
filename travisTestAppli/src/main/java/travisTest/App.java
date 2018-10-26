15df1d13754d2ccc1ad95c7508b6b8a60e39b291
Vanmarcke Timothey <vanmarcke@a14p2.fil.univ-lille1.fr>
2018-10-26 08:34:49 +0200

    Initialisation avec un Hello World

---
+++ travisTestAppli/src/main/java/travisTest/App.java
@@ -0,0 +1,13 @@
+package travisTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
