eb1e9037e5b88233f4018a912feab0bc70caac20
Vanmarcke Timothey <vanmarcke@a14p2.fil.univ-lille1.fr>
2018-10-26 09:57:42 +0200

    Ajout  modification App

--- src/main/java/travisTest/App.java
+++ src/main/java/travisTest/App.java
@@ -1,6 +1,7 @@
 package travisTest;
 
 /**
+fzefze
  * Hello world!
  *
  */
@@ -9,5 +10,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	int i = 1;
+	int j = 2;
+	x = i+j;
+	r = x;
+
+
+
+
+
+	String hello = "Hello";
+
     }
 }
